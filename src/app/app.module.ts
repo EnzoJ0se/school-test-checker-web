436937e8d23db18a52b15cad495513e0f89df4dc
Enzo Jose <enzojose.reus@gmail.com>
2024-06-19 23:52:29 -0300

    build: add plugin and change configs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { BrowserModule } from "@angular/platform-browser";
 import { LoginModule } from "./pages/login/login.module";
 import { HomeModule } from "./pages/home/home.module";
 import { DefaultPageContainerModule } from "./pages/default-page-container/default-page-container.module";
+import { AngularSvgIconModule, provideAngularSvgIcon } from "angular-svg-icon";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
     imports: [
@@ -14,9 +16,7 @@ import { DefaultPageContainerModule } from "./pages/default-page-container/defau
         RouterOutlet,
         CommonModule,
         AppRoutingModule,
-        // LoginModule,
-        // HomeModule,
-        DefaultPageContainerModule
+        DefaultPageContainerModule,
     ],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
