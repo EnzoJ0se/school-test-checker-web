68870635fb15a5cf4daf68d2c87c188b83ae9d28
Enzo Jose <enzojose.reus@gmail.com>
2024-09-29 18:24:04 -0300

    feat: layout improvements

--- src/app/pages/registration/test/test-form/test-questions-form/test-questions.form.component.ts
+++ src/app/pages/registration/test/test-form/test-questions-form/test-questions.form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { AfterViewInit, ChangeDetectorRef, Component, Input } from "@angular/core";
 import { TestDTO } from "../../../../../resources/test/test.dto";
 import { ControlContainer, NgForm } from "@angular/forms";
 import { TestQuestionDTO } from "../../../../../resources/test-question/test-question.dto";
@@ -15,17 +15,27 @@ import { uuid } from "../../../../../helper/form-helpers";
         }
     ],
 })
-export class TestQuestionsFormComponent {
+export class TestQuestionsFormComponent implements AfterViewInit {
     @Input() public testDTO: TestDTO = new TestDTO();
+
+    public openIndex: number | null = 0;
+
+    public constructor(private changeDetector: ChangeDetectorRef) {}
+
+    public ngAfterViewInit(): void {
+        if (!this.testDTO.test_questions?.length) {
+            this.addQuestion();
+        }
+    }
 
     public addQuestion(): void {
         if (!this.testDTO?.test_questions?.length) {
             this.testDTO.test_questions = [];
         }
 
-        this.testDTO.test_questions.push(new TestQuestionDTO({
-            identifier: this.testDTO.test_questions.length + 1,
-        }));
+        this.testDTO.test_questions.push(new TestQuestionDTO({ identifier: this.testDTO.test_questions.length + 1 }));
+        this.openIndex = this.testDTO.test_questions.length - 1;
+        this.changeDetector.detectChanges();
     }
 
     public removeQuestion(index: number): void {
